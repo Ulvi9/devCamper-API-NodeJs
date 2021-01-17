6c6055f84aa96bdec20e0237ca43a1b08ba9a8bf
ulvi macid <ulvizm@code.edu.az>
2021-01-18 00:11:58 +0400

    bootcamp model CRUD

--- DevCamperApi/controllers/bootcamps.js
+++ DevCamperApi/controllers/bootcamps.js
@@ -3,20 +3,59 @@ const Bootcamp=require("../models/Bootcamp");
 //@desc get all bootcamps
 //@route GET api/v1/bootcamps
 //@access public
-exports.getBootcamps=(req,res,next)=>{
-    res.status(200).json({success:true,msg:"show all bootcamps"})
+exports.getBootcamps=async (req,res,next)=>{
+    try {
+        const bootcamps=await Bootcamp.find();
+        res.status(200).json({
+            count:bootcamps.length,
+            success:true,
+            data:bootcamps
+        })
+    } catch (e) {
+        res.status(400).json({
+            success:false,
+        })
+    }
+
 }
 //@desc get  bootcamp
 //@route GET api/v1/bootcamps/:id
 //@access public
-exports.getBootcamp=(req,res,next)=>{
-    res.status(200).json({success:true,msg:"show bootcamp"})
+exports.getBootcamp=async (req,res,next)=>{
+    try {
+        const bootcamp=await Bootcamp.findById(req.params.id);
+        if (!bootcamp){
+            return res.status(400).json({success:false})
+        }
+        res.status(200).json({
+            success:true,
+            data:bootcamp
+        })
+    } catch (e) {
+        res.status(400).json({
+            success:false,
+        })
+    }
 }
 //@desc get all bootcamps
 //@route PUT api/v1/bootcamps/:id
 //@access public
-exports.editBootcamp=(req,res,next)=>{
-    res.status(200).json({success:true,msg:"edit  bootcamp"})
+exports.editBootcamp=async (req,res,next)=>{
+   try {
+       const bootcamp=await Bootcamp.findByIdAndUpdate(req.params.id,req.body,{
+           new:true,
+           runValidators:true
+       })
+       if (!bootcamp){
+           res.status(400).json({success:false});
+       }
+       res.status(200).json({
+           success:true,
+           data:bootcamp
+       })
+   } catch (e) {
+       res.status(400).json({success:false});
+   }
 }
 //@desc get all bootcamps
 //@route POST api/v1/bootcamps
@@ -38,6 +77,17 @@ exports.createBootcamp=async (req,res,next)=>{
 //@desc get all bootcamps
 //@route DELETE api/v1/bootcamps/:id
 //@access public
-exports.deleteBootcamp=(req,res,next)=>{
-    res.status(200).json({success:true,msg:"delete bootcamp"})
+exports.deleteBootcamp=async (req,res,next)=>{
+    try {
+        const bootcamp=await Bootcamp.findByIdAndDelete(req.params.id)
+        if (!bootcamp){
+            res.status(400).json({success:false});
+        }
+        res.status(200).json({
+            success:true,
+            data:{}
+        })
+    } catch (e) {
+        res.status(400).json({success:false});
+    }
 }
